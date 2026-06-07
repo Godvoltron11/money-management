b64752efa13d23b6ef6384e3dd16d6967afd0caf
Godvoltron11 <sameer.vv91@gmail.com>
2026-06-07 09:32:32 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+
+function App() {  return (
+    <div className="App">
+      <h1>Welcome to React with TypeScript!</h1>
+      <p>This is a simple React application using TypeScript.</p>
+    </div>
+  )
+}
+
+export default App
